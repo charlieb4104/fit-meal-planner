e8572b05840619e528a3da4d3b6b3204af5f24dc
Charlie Broadbent <charliebroadbent@Charlies-MacBook-Air.local>
2026-07-17 15:30:52 +0100

    Added workout plan and summary page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import './App.css'
 
 function App() {
   const [start, setStarted] = useState(false)
+  const [selectedDays, setSelectedDays] = useState<string[]>([])
+  const [enter, setEnter] = useState(false)
+
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday" 
+  ];
+
 
   if (!start){
     return(
@@ -13,7 +26,39 @@ function App() {
     );
   }
 
+  let workoutPlan: string[] = [];
 
+  if (selectedDays.length === 3){
+    workoutPlan = ["Upper Body", "Lower Body", "Full Body and Cardio"];
+  } else if (selectedDays.length === 4){
+    workoutPlan = [
+      "Upper Body", 
+      "Lower Body", 
+      "Full Body and Cardio", 
+      "Core"];
+  } else if (selectedDays.length >= 5){
+    workoutPlan = [
+      "Upper Body", 
+      "Lower Body", 
+      "Full Body", 
+      "Cardio",
+      "Core"
+    ];
+  }
+
+  if(enter){
+    return(
+      <div>
+        <h1>Your Workout Plan</h1>
+
+        {selectedDays.map((day, index) => (
+          <p key={day}>
+            {day} - {workoutPlan[index] || "Cardio"}
+          </p>
+        ))}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -23,22 +68,32 @@ function App() {
           <p>
             Your workouts and meals all in one place. Isn't that proper fit. 
           </p>
+          <br />
           <h2>Workout Preferences</h2>
-          <label>How many days per week?</label>
-          <input type="number"/>
+          <br/>
+          <label>Select workout days</label>
+          <br />
+          {days.map((day) => (
+            <div key={day}>
+              <input
+                type="checkbox"
+                id={day}
+                checked={selectedDays.includes(day)}
+                onChange={() => {
+                  if (selectedDays.includes(day)) {
+                    setSelectedDays(selectedDays.filter((d) => d !== day));
+                  } else {
+                    setSelectedDays([...selectedDays, day]);
+                  }
+                }}
+              />
+              <label htmlFor={day}>{day}</label>
+            </div>
+          ))}
           <br />
           <br />
-
-          <label>
-            Goal
-          </label>
-
-          <select>
-            <option>Build Muscle</option>
-            <option>Lose Fat</option>
-            <option>Maintain Weight</option>
-          </select>
         </div>
+        <button onClick={() => setEnter(true)}>Generate my plan!</button>
       </section>
     </>
   );
